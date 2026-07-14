59be84fb1b2b00d85d6cdd2aeb77b3ac734abe80
Rookie Nguyen <nhqthang@proton.me>
2026-07-14 23:49:16 +0700

    chore: fuck you cors

--- server/config/settings.py
+++ server/config/settings.py
@@ -52,7 +52,15 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+] + os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+
+CORS_ALLOW_CREDENTIALS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
