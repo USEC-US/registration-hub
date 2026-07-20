caad3b36861b9ffcce47a6aa4f5b2f257cd6e351
Rookie Nguyen <nhqthang@proton.me>
2026-07-20 15:40:51 +0700

    chore: temporarily proxy the admin and static route to SvelteKit
    Note: eventually a proper admin/profile panel will be built in SvelteKit to handle more things than just view the data.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -54,5 +54,18 @@ export default defineConfig({
 				}
 			}
 		]
-	}
+	},
+  server: {
+    proxy: {
+      "/admin": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        secure: false
+      },
+      "/static": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true
+      }
+    }
+  }
 });
